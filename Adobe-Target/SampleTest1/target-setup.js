9e5d0ddfa3b26278dafb863c44ab060543d3cb77
PoojithaAdobe19 <poojitha.adobe19@gmail.com>
2024-08-21 20:27:54 -0400

    Cleaned up directory structure, removed nested directories

--- Adobe-Target/SampleTest1/target-setup.js
+++
@@ -1,9 +0,0 @@
-// Adobe Target - Sample A/B Test Setup
-adobe.target.trackEvent({
-    mbox: "product-page-promo",
-    parameters: {
-        "experience": "A/B Test 1",
-        "offer-A": "Get 10% off your first order!",
-        "offer-B": "Get 20% off your first order!"
-    }
-});
